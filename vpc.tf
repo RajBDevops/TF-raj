c151af1ecf22a8c3e1ea4ca46670a7ca23dfbcd8
RajBDevops <rajkumar.bandaru1987@gmail.com>
2025-01-27 10:43:34 +0530

    Createing  Subnet

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,32 @@ resource "aws_vpc" "lms-vpc" {
     Name = "lms-vpc"
   }
 }
+#web subnet
+resource "aws_subnet" "Lms-web-sn" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Lms-Web-subnet"
+  }
+}
+#Api subnet
+resource "aws_subnet" "lms-Api-sn" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "Lms-Api-subnet"
+  }
+}
+#database subnet
+resource "aws_subnet" "lms-db-sn" {
+  vpc_id     = aws_vpc.lms-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "Lms-database-subnet"
+  }
+}
